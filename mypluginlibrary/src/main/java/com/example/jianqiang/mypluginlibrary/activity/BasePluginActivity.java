9d85e247ef3e6e5deeafb76e2aa1f42903ced1af
1835434698 <1835434698@qq.com>
2021-03-09 16:37:34 +0800

    Update BasePluginActivity.java
    无侵入启动activity

--- mypluginlibrary/src/main/java/com/example/jianqiang/mypluginlibrary/activity/BasePluginActivity.java
+++ mypluginlibrary/src/main/java/com/example/jianqiang/mypluginlibrary/activity/BasePluginActivity.java
@@ -1,12 +1,12 @@
 package com.example.jianqiang.mypluginlibrary.activity;
 
-import android.app.Activity;
+import android.view.ContextThemeWrapper;
 import android.content.Intent;
 import android.content.res.Resources;
 import android.os.Bundle;
 import android.view.View;
 
-public class BasePluginActivity extends Activity implements IRemoteActivity {
+public class BasePluginActivity extends ContextThemeWrapper implements IRemoteActivity {
 
     private static final String TAG = "Client-BaseActivity";
 
@@ -17,6 +17,10 @@ public class BasePluginActivity extends Activity implements IRemoteActivity {
     protected Activity that;
     protected String dexPath;
 
+    public String getPackageName(){
+        Log.d(TAG, "111111 getPackageName  = "+that.getPackageName());
+        return that.getPackageName();
+    }
 
     private int launchMode = LaunchMode.STANDARD;
 
@@ -79,14 +83,42 @@ public class BasePluginActivity extends Activity implements IRemoteActivity {
         return that.findViewById(id);
     }
 
-    @Override
-    public void startActivity(Intent intent) {
+//    @Override
+    public void startActivity(Intent data) {
+        Log.d(TAG, "path startActivity ");
+        Intent intent = replaceIntent(data);
         that.startActivity(intent);
     }
 
-    @Override
-    public void startActivityForResult(Intent intent, int requestCode) {
+//    @Override
+    public void startActivityForResult(Intent data, int requestCode) {
+        Intent intent = replaceIntent(data);
         that.startActivityForResult(intent, requestCode);
+    }
+
+    private Intent replaceIntent(Intent data) {
+        Log.d(TAG, "111");
+
+        if (!TextUtils.isEmpty(data.getAction())){
+            if (TextUtils.isEmpty(data.getStringExtra(AppConstants.EXTRA_DEX_PATH))) {
+                data.putExtra(AppConstants.EXTRA_DEX_PATH, dexPath);
+            }
+            return data;
+        }else {
+            if (!TextUtils.isEmpty(data.getComponent().getClassName()) && data.getComponent().getClassName().contains(data.getComponent().getPackageName())){
+                return data;
+            }else {
+                Intent intent = new Intent(AppConstants.PROXY_VIEW_ACTION);
+                intent.putExtra(AppConstants.EXTRA_DEX_PATH, dexPath);
+                Log.d(TAG, "path getClassName = " + data.getComponent().getClassName());
+                intent.putExtras(data);
+                intent.putExtra(AppConstants.EXTRA_CLASS, data.getComponent().getClassName());
+                String userName = intent.getStringExtra("userName");
+                Log.d(TAG, " userName = "+userName);
+                return intent;
+            }
+
+        }
     }
 
     @Override
@@ -102,5 +134,13 @@ public class BasePluginActivity extends Activity implements IRemoteActivity {
     @Override
     public Intent getIntent() {
         return that.getIntent();
+    }
+    
+    public void onBackPressed() {
+        that.onBackPressed();
+    }
+
+    public final void setResult(int resultCode, Intent data) {
+        that.setResult(resultCode, data);
     }
 }
